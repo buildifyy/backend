b38800fc63c684d4bb408fc306c47ac995249518
Varun Gupta <varungupta2015135@gmail.com>
2023-05-01 18:48:04 +0530

    renamed controller and started settings for database

--- buildify-backend/Controllers/SampleProductController.cs
+++ buildify-backend/Controllers/SampleProductController.cs
@@ -7,17 +7,12 @@ namespace buildify_backend.Controllers;
 
 [ApiController]
 [Route("/products")]
-public class WeatherForecastController : ControllerBase
+public class SampleProductController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<SampleProductController> _logger;
     private readonly IRepository _repository;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger, IRepository repository)
+    public SampleProductController(ILogger<SampleProductController> logger, IRepository repository)
     {
         _logger = logger;
         _repository = repository;
